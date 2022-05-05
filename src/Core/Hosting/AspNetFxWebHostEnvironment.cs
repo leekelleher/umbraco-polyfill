bc8e6a9a0bf4a91573ba18617bd6c98b34ea0ae6
leekelleher <leekelleher@gmail.com>
2022-05-05 13:45:49 +0100

    Added `IWebHostEnvironment` polyfill
    Minimal implementation for `MapPathWebRoot` method.

---
+++ src/Core/Hosting/AspNetFxWebHostEnvironment.cs
@@ -0,0 +1,15 @@
+﻿#if NET472
+using Microsoft.AspNetCore.Hosting;
+using Umbraco.Core.IO;
+
+namespace Umbraco.Core.Hosting
+{
+    internal sealed class AspNetFxWebHostEnvironment : IWebHostEnvironment
+    {
+        public string MapPathWebRoot(string path)
+        {
+            return IOHelper.MapPath(path);
+        }
+    }
+}
+#endif
